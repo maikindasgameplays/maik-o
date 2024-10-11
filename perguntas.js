01d0cd8c591b60c756dd71ccb640cadb3cfd6b75
maikindasgameplays <143224497+maikindasgameplays@users.noreply.github.com>
2024-10-11 09:35:12 -0300

    d

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,126 @@
-CriaCartao
-    'Geografia'
-    ''
+CriaCartao(
+    'História',
+    'A Política de Distensão, levada a cabo pelo General Ernesto Geisel, visava',
+    'acalmar a tensão política entre Governo e Oposição.'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'História',
+    'Qual foi o impacto da Revolução Industrial?',
+    'Transformou economias agrícolas em industriais.'
+    
+
+)
+CriaCartao(
+    'Matemática',
+    'O que é o teorema de Pitágoras?',
+    '"A relação entre os lados de um triângulo retângulo.'
+    
+
+)
+CriaCartao(
+    'Biologia',
+    'Como funciona a fotossíntese?',
+    'As plantas convertem luz solar em energia'
+    
+
+)
+    
+CriaCartao(
+    'Química',
+    'O que é uma reação química?',
+    'Transformação de substâncias em novas substâncias'
+
+)
+CriaCartao(
+    'Física',
+    'Qual é a lei da gravitação universal',
+    'Todo corpo atrai outro com uma força proporcional à massa'
+
+)
+CriaCartao(
+    'Geografia',
+    'Quais são as camadas da Terra?',
+    'Crosta, manto e núcleo.'
+
+)
+CriaCartao(
+    'Literatura',
+    'Qual a principal obra de Machado de Assis?',
+    'Dom Casmurro.'
+
+)
+CriaCartao(
+    'Sociologia',
+    'O que é cultura?',
+    'Conjunto de hábitos e valores de um grupo'
+
+)
+CriaCartao(
+    'Filosofia',
+    'O que defendia Platão?',
+    'A teoria das ideias e a busca pela verdade.'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+CriaCartao(
+    'Geografia',
+    'A Campanha das "Diretas-Já" foi um episódio marcante ocorrido na década de 1980, constituindo um dos significativos momentos do processo histórico brasileiro conhecido como:',
+    'redemocratização'
+
+)
+
+
+
+
+
+
+
+    
