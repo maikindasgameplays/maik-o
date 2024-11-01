7bfcdf562fe4cba0f336363787aaa74ce0008277
maikindasgameplays <143224497+maikindasgameplays@users.noreply.github.com>
2024-11-01 12:18:24 -0300

    th

--- pag2.js
+++ pag2.js
@@ -3,7 +3,7 @@ function CriaCartao(categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="pergunta-cartao" onclick="MostrarRresposta"(this)>
+    <div class="pergunta-cartao" onclick="MostrarRresposta(this)">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao"
             <p>${pergunta}</p>
@@ -18,10 +18,10 @@ function CriaCartao(categoria, pergunta, resposta){
     function MostrarRresposta(card){
         const resposta = card.querySelector('.resposta-cartao');
         if(resposta.style.display === 'none'){
-            resposta.style.display = 'block'
+            resposta.style.display = 'block';
         }
         else{
-            resposta.style.display = 'none'
+            resposta.style.display = 'none';
 
         }
     }
